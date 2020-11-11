9b227bf535faa06747c8a1dc4bcb226d8ca5805a
maritox09 <41440214+maritox09@users.noreply.github.com>
2020-11-11 14:49:05 -0600

    Update confirmarorden.php

--- auxis/confirmarorden.php
+++ auxis/confirmarorden.php
@@ -81,7 +81,7 @@ if(mysqli_query($con,"DELETE FROM carrito")){
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'jmyc9999@gmail.com';                     // SMTP username
-        $mail->Password   = 'Chema$899';                               // SMTP password
+        $mail->Password   = '----';                              // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
